9c04783d19bdf8a58bd366f42e955d96c0522ea1
Gordon Wrigley <gordon.wrigley@gmail.com>
2018-03-05 21:51:24 +0000

    bigger starting window

--- main.py
+++ main.py
@@ -927,7 +927,7 @@ def setup():
 
 
 def main():
-    window = Window(width=800, height=600, caption='Pyglet', resizable=True)
+    window = Window(width=1500, height=1000, caption='Pyglet', resizable=True)
     # Hide the mouse cursor and prevent the mouse from leaving the window.
     window.set_exclusive_mouse(True)
     setup()
